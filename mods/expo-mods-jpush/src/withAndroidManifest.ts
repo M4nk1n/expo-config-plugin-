cafaf6e1e15b9244a9e6be787b849fca20589e49
Mankin <vinkencheng@gmail.com>
2023-01-10 17:20:02 +0800

    fix: fixed Android JPush Permission and remove JNotifyActivity service

--- mods/expo-mods-jpush/src/withAndroidManifest.ts
+++ mods/expo-mods-jpush/src/withAndroidManifest.ts
@@ -15,6 +15,7 @@ export const setAndroidManifest: ConfigPlugin = config =>
         "${JPUSH_CHANNEL}"
       )
     }
+
     if (AndroidConfig.Manifest.findMetaDataItem(config.modResults.manifest.application[0], "JPUSH_APPKEY") === -1) {
       console.log("\n[JPushExpoConfigPlugin] 配置 AndroidManifest JPUSH_APPKEY ... ")
       AndroidConfig.Manifest.addMetaDataItemToMainApplication(
@@ -28,44 +29,45 @@ export const setAndroidManifest: ConfigPlugin = config =>
       return config
     }
 
-    if (
-      config.modResults.manifest.application[0].activity.findIndex(
-        item => item.$["android:name"] === "cn.jpush.android.service.JNotifyActivity"
-      ) === -1
-    ) {
-      console.log("\n[JPushExpoConfigPlugin] 此后两项为BUG修复, 对应版本 jpush-react-native 2.8.3")
-      console.log("\n[JPushExpoConfigPlugin] - 配置 AndroidManifest xmlns:tools ... ")
+    // 插件中已有此部份配置
+    // if (
+    //   config.modResults.manifest.application[0].activity.findIndex(
+    //     item => item.$["android:name"] === "cn.jpush.android.service.JNotifyActivity"
+    //   ) === -1
+    // ) {
+    //   console.log("\n[JPushExpoConfigPlugin] 此后两项为BUG修复, 对应版本 jpush-react-native 2.8.3")
+    //   console.log("\n[JPushExpoConfigPlugin] - 配置 AndroidManifest xmlns:tools ... ")
 
-      config.modResults.manifest.$["xmlns:tools"] = "http://schemas.android.com/tools"
+    //   config.modResults.manifest.$["xmlns:tools"] = "http://schemas.android.com/tools"
 
-      console.log("\n[JPushExpoConfigPlugin] - 配置 AndroidManifest activity ... ")
-      config.modResults.manifest.application[0].activity.push({
-        $: {
-          "android:name": "cn.jpush.android.service.JNotifyActivity",
-          "android:exported": "true",
-          "tools:node": "replace",
-          "android:taskAffinity": "jpush.custom",
-          "android:theme": "@android:style/Theme.Translucent.NoTitleBar"
-        },
-        "intent-filter": [
-          {
-            action: [
-              {
-                $: { "android:name": "cn.jpush.android.intent.JNotifyActivity" }
-              }
-            ],
-            category: [
-              {
-                $: { "android:name": "android.intent.category.DEFAULT" }
-              },
-              {
-                $: { "android:name": "${applicationId}" }
-              }
-            ]
-          }
-        ]
-      })
-    }
+    //   console.log("\n[JPushExpoConfigPlugin] - 配置 AndroidManifest activity ... ")
+    //   config.modResults.manifest.application[0].activity.push({
+    //     $: {
+    //       "android:name": "cn.jpush.android.service.JNotifyActivity",
+    //       "android:exported": "true",
+    //       "tools:node": "replace",
+    //       "android:taskAffinity": "jpush.custom",
+    //       "android:theme": "@style/JPushTheme"
+    //     },
+    //     "intent-filter": [
+    //       {
+    //         action: [
+    //           {
+    //             $: { "android:name": "cn.jpush.android.intent.JNotifyActivity" }
+    //           }
+    //         ],
+    //         category: [
+    //           {
+    //             $: { "android:name": "android.intent.category.DEFAULT" }
+    //           },
+    //           {
+    //             $: { "android:name": "${applicationId}" }
+    //           }
+    //         ]
+    //       }
+    //     ]
+    //   })
+    // }
 
     return config
   })
--- mods/expo-mods-jpush/src/withJPush.ts
+++ mods/expo-mods-jpush/src/withJPush.ts
@@ -1,4 +1,4 @@
-import { ConfigPlugin, createRunOncePlugin } from "@expo/config-plugins"
+import { AndroidConfig, ConfigPlugin, createRunOncePlugin } from "@expo/config-plugins"
 
 import { setAppBuildGradle, setSettingsGradle } from "./withAndroidGradle"
 import { setAndroidManifest } from "./withAndroidManifest"
@@ -13,6 +13,11 @@ const withJPush: ConfigPlugin<{ appKey: string; channel: string } | void> = (con
   if (!props || !props.appKey || !props.channel) {
     throw new Error("[JPushExpoConfigPlugin] 请传入参数 appKey & channel")
   }
+
+  // Android
+  config = AndroidConfig.Permissions.withPermissions(config, [
+    "android.permission.POST_NOTIFICATIONS" // since Android 13
+  ])
   config = setAppDelegateHeader(config)
   config = setAppDelegate(config, props)
   config = setAndroidManifest(config)
